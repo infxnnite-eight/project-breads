6737e4e8e0f47e2672393b936bdcafc54384e0dd
infxnnite8 <dorianquintana@gmail.com>
2023-10-24 16:00:48 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
   
-  // Breads
+// Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+ 
   
 
 // LISTEN
